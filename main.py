cb86cfb3065bd3aae4ee7040cae67a0dbea78366
SakiKawasaki <theniklick@gmail.com>
2020-03-22 22:02:33 +0700

    fix main.py

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
-<<<<<<< HEAD
-from InstagramAPI import InstagramAPI
-=======
 # -*- coding: utf-8 -*-
+from InstagramAPI import InstagramAPI
 from libs.pic import Picture
->>>>>>> 1be61277d29a406ee86aeffce6017dfaa6f112f2
 
 InstagramAPI = InstagramAPI("_ganjaclub", "lolka228")
 InstagramAPI.login()  # login
 
-<<<<<<< HEAD
-photo_path = 'fon.jpg'
+photo_path = "back.png"
 caption = "Sample photo"
 InstagramAPI.uploadPhoto(photo_path, caption=caption)
-=======
+
 Picture.create("filename.png", "virgo")
->>>>>>> 1be61277d29a406ee86aeffce6017dfaa6f112f2
