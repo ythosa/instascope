c6f1a43dd7058721536b86d3c56e88900573e0a1
bigboi300 <vasus714@yandex.ru>
2020-03-22 16:46:42 +0300

    creating pic for story

---
+++ libs/pic.py
@@ -0,0 +1,23 @@
+from PIL import Image, ImageDraw, ImageFont
+
+from horoscope import Horoscope
+from meme import Meme
+
+# take a horoscope list
+horoscope_list = Horoscope.get_horoscope()
+# take a meme
+Meme.get()
+
+
+fon = Image.new('RGB', (1080, 1920), color='#FFFFFF')
+font = ImageFont.truetype(r'data\DroidSans.ttf', 140)
+meme = Image.open('meme.png', 'r')
+meme = meme.resize((1000, 1000))
+
+fon.save('fon.jpg')
+story_img = ImageDraw.Draw(fon)
+
+story_img.text((100, 100), horoscope_list['leo'][0], align="left", font=font, fill="#000000")
+fon.paste(meme, (40, 880))
+
+fon.show()
