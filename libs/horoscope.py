5a8879f9aefc6a1289b7e262b8d162a335622761
bigboi300 <vasus714@yandex.ru>
2020-03-22 14:23:31 +0300

    add horoscope parser

--- libs/horoscope.py
+++ libs/horoscope.py
@@ -1,1 +1,32 @@
 # -*- coding: utf-8 -*-
+
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+
+class Horoscope:
+    @staticmethod
+    def get_horoscope():
+        # URL of horoscope site
+        url = "https://1001goroskop.ru/?znak="
+        # Horoscope symbols
+        symbols = {
+            "taurus": ["Телец", ""],
+            "aries": ["Овен", ""],
+            "gemini": ["Близнецы", ""],
+            "cancer": ["Рак", ""],
+            "leo": ["Лев", ""],
+            "libra": ["Весы", ""],
+            "sagittarius": ["Стрелец", ""],
+            "capricorn": ["Козерог", ""],
+            "aquarius": ["Водолей", ""],
+            "pisces": ["Рыбы", ""],
+            "virgo": ["Дева", ""],
+            "scorpio": ["Скорпион", ""],
+        }
+        # Parse text
+        for symb, text in symbols.items():
+            html_doc = urlopen(url + symb).read()
+            soup = BeautifulSoup(html_doc, features="html.parser")
+            text[1] = str(soup.find('p'))[3:-4]
+        return symbols
