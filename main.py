4ef8f46cdef2422905f752662703f18ccc510c87
SakiKawasaki <theniklick@gmail.com>
2020-03-22 22:10:06 +0700

    update main.py

--- main.py
+++ main.py
@@ -2,12 +2,13 @@
 from InstagramAPI import InstagramAPI
 from libs.pic import Picture
 
-photo_path = "back.png"
+path = "back.png"
 
-Picture.create(photo_path, "virgo")
+Picture.create(path, "virgo")
 
+# --- Auth --- #
 InstagramAPI = InstagramAPI("_ganjaclub", "lolka228")
 InstagramAPI.login()  # login
 
-caption = "Sample photo"
-InstagramAPI.uploadPhoto(photo_path, caption=caption)
+# --- Upload --- #
+InstagramAPI.uploadStoryPhoto(path)
