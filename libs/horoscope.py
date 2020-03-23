b9f86594288fe2d731e6b6d8d7a4c62a4b23128c
SakiKawasaki <theniklick@gmail.com>
2020-03-23 15:07:23 +0700

    update horoscope.py

--- libs/horoscope.py
+++ libs/horoscope.py
@@ -6,6 +6,8 @@ from urllib.request import urlopen
 
 
 class Horoscope:
+    # URL of horoscope site
+    url = "https://1001goroskop.ru/?znak="
     symbols = {
         "taurus": ["Телец", ""],
         "aries": ["Овен", ""],
@@ -21,15 +23,11 @@ class Horoscope:
         "scorpio": ["Скорпион", ""],
     }
 
-    @classmethod
-    def get_horoscope(cls, symb="leo"):
-        # URL of horoscope site
-        url = "https://1001goroskop.ru/?znak="
-        # Horoscope symbols
-        
+    @staticmethod
+    def get_horoscope(symb="leo"):
         # Parse text
-        html_doc = urlopen(url + symb).read()
+        html_doc = urlopen(Horoscope.url + symb).read()
         soup = BeautifulSoup(html_doc, features="html.parser")
         soup = str(soup.find('p'))[3:-4]
-        cls.symbols[symb][1] = "".join(re.split(r"([\!\?\.]+)", soup, 3)[:4])
-        return cls.symbols
+        Horoscope.symbols[symb][1] = "".join(re.split(r"([\!\?\.]+)", soup, 3)[:4])
+        return Horoscope.symbols
