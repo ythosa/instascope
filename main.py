774466996ee49e4cdf468e9411da9ce9161eddd7
bigboi300 <vasus714@yandex.ru>
2020-03-24 18:32:26 +0300

    fix small bug

--- main.py
+++ main.py
@@ -24,39 +24,41 @@ bot = telegram.Bot(token='1085045815:AAESWK5yzQTTsjDWBzkvYwdrkVK9rUgLAoQ')
 
 # User Response Function
 def generate_answer(text, chat_id):
-    if text == '/help':
-        bot.send_message(chat_id=chat_id, text="Write <horoscope> to get horoscopes for all signs.\n"
-                                               "Write <horoscope some_sign> to get horoscope for this sign.\n"
-                                               "Write </signs> to get list of all signs.")
-    elif text == '/signs':
-        str_signs = ""
-        for k, v in symbols.items():
-            str_signs += "".join(str(k)+' - '+str(v)+'\n')
-        bot.send_message(chat_id=chat_id, text=str_signs)
-    else:
-        text = text.rstrip().split(' ')
-        if text[0] == 'horoscope':
-            if len(text) == 2:
-                if text[1] in symbols.values():
-                    # return pic with this sign
-                    sign = str(text[1])
-                    name_of_pic = '_'+sign+'.png'
-                    Picture.create(name_of_pic, sign)
-                    bot.send_document(chat_id=chat_id, document=open(name_of_pic, 'rb'))
+    text = text.rstrip()
+    if len(text) > 0:
+        if text == '/help':
+            bot.send_message(chat_id=chat_id, text="Write <horoscope> to get horoscopes for all signs.\n"
+                                                   "Write <horoscope some_sign> to get horoscope for this sign.\n"
+                                                   "Write </signs> to get list of all signs.")
+        elif text == '/signs':
+            str_signs = ""
+            for k, v in symbols.items():
+                str_signs += "".join(str(k)+' - '+str(v)+'\n')
+            bot.send_message(chat_id=chat_id, text=str_signs)
+        else:
+            text = text.split(' ')
+            if text[0] == 'horoscope':
+                if len(text) == 2:
+                    if text[1] in symbols.values():
+                        # return pic with this sign
+                        sign = str(text[1])
+                        name_of_pic = '_'+sign+'.png'
+                        Picture.create(name_of_pic, sign)
+                        bot.send_document(chat_id=chat_id, document=open(name_of_pic, 'rb'))
+                    else:
+                        # return wrong
+                        bot.send_message(chat_id=chat_id, text="Invalid request")
+                elif len(text) == 1:
+                    # generate pic for all sings
+                    for sign in symbols.values():
+                        Picture.create(name='_' + str(sign) + '.png', z=sign)
+                        bot.send_document(chat_id=chat_id, document=open('_' + str(sign) + '.png', 'rb'))
                 else:
                     # return wrong
                     bot.send_message(chat_id=chat_id, text="Invalid request")
-            elif len(text) == 1:
-                # generate pic for all sings
-                for sign in symbols.values():
-                    Picture.create(name='_' + str(sign) + '.png', z=sign)
-                    bot.send_document(chat_id=chat_id, document=open('_' + str(sign) + '.png', 'rb'))
             else:
                 # return wrong
                 bot.send_message(chat_id=chat_id, text="Invalid request")
-        else:
-            # return wrong
-            bot.send_message(chat_id=chat_id, text="Invalid request")
 
 
 last_upd = 0
